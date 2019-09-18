1fd1f735100ba243c778213c570da5ebc51dad99
Tim Brunson <tjbrunson@gmail.com>
2019-09-18 07:57:18 -0700

    minor changes

--- src/main/java/org/reactome/web/idg/client/IDGBrowser.java
+++ src/main/java/org/reactome/web/idg/client/IDGBrowser.java
@@ -1,8 +1,13 @@
 package org.reactome.web.idg.client;
 
+import org.reactome.web.diagram.client.DiagramFactory;
+import org.reactome.web.fi.client.IdgDiagramViewerImpl;
 import org.reactome.web.pwp.client.Browser;
 
-
 public class IDGBrowser extends Browser {
-
+	
+	static {
+        DiagramFactory.setDiagramViewerCreator(() -> new IdgDiagramViewerImpl());
+    }
+	
 }
