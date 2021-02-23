828f8af6e309ba0fc1914d7a3bc06aa5d849332a
Tim Brunson <tjbrunson@gmail.com>
2021-02-23 13:54:35 -0800

    hide controls when not flagging interactors

--- src/main/java/org/reactome/web/idg/client/fireworks/IDGFlaggedItemsControl.java
+++ src/main/java/org/reactome/web/idg/client/fireworks/IDGFlaggedItemsControl.java
@@ -10,6 +10,9 @@ import org.reactome.web.idg.client.fireworks.events.SetFIFlagDataDescKeysEvent;
 import org.reactome.web.idg.client.fireworks.handlers.SetFIFlagDataDescKeysHandler;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.Style.Float;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
@@ -17,6 +20,8 @@ import com.google.gwt.event.shared.EventBus;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.TextBox;
 
 /**
@@ -27,17 +32,25 @@ import com.google.gwt.user.client.ui.TextBox;
 public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFIFlagDataDescKeysHandler{
 
 	private List<Integer> dataDescKeys;
+	private FlowPanel prdPanel;
 	private TextBox prdInput;
+	private FlowPanel fdrPanel;
+	private TextBox fdrInput;
+	private FlowPanel controlPanel;
 	
 	public IDGFlaggedItemsControl(EventBus eventBus) {
 		super(eventBus);
 		
 		super.selector.removeFromParent();
+		this.getElement().getStyle().setHeight(56, Unit.PX);
 		
 		//Remove default style and make the msgLabel wider
 		//Required because the default width is tagged with !important
 		this.msgLabel.removeStyleName(msgLabel.getStyleName());
-		this.msgLabel.addStyleName(IDGRESOURCES.getCSS().idgFlaggedItemsLabel());
+		
+		controlPanel = new FlowPanel();
+		controlPanel.getElement().getStyle().setFloat(Float.LEFT);
+		controlPanel.getElement().getStyle().setWidth(100, Unit.PCT);
 		
 		prdInput = new TextBox();
 		
@@ -45,11 +58,9 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 		prdInput.getElement().setAttribute("type", "number");
 		prdInput.getElement().setAttribute("min", "0");
 		prdInput.getElement().setAttribute("max", "1");
-		prdInput.getElement().setAttribute("step", "0.1");
 		
 		prdInput.setStyleName(IDGRESOURCES.getCSS().prdInput());
 		prdInput.addKeyDownHandler(new KeyDownHandler() {
-
 			@Override
 			public void onKeyDown(KeyDownEvent event) {
 				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
@@ -59,8 +70,43 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 		});
 		
 		prdInput.setValue("0.9"); //set default PRD value to 0.9
-		super.add(prdInput);
-		prdInput.setVisible(false);
+		InlineLabel prdLbl = new InlineLabel("FI Score ≥");
+		prdLbl.getElement().getStyle().setFloat(Float.NONE);
+		prdPanel = new FlowPanel();
+		prdPanel.getElement().getStyle().setFloat(Float.LEFT);
+		prdPanel.add(prdLbl);
+		prdPanel.add(prdInput);
+		controlPanel.add(prdPanel);
+		prdInput.setVisible(true);
+		
+		//make fdr input box
+		fdrInput = new TextBox();
+		
+		//seting attributes on TexBox to make it function better
+		fdrInput.getElement().setAttribute("type", "number");
+		fdrInput.getElement().setAttribute("min", "0");
+		fdrInput.getElement().setAttribute("max", "1");
+		fdrInput.setStyleName(IDGRESOURCES.getCSS().prdInput());
+		fdrInput.addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if(event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					updateFDR();
+				}
+			}
+		});
+		
+		fdrInput.setValue("0.05");
+		InlineLabel fdrLbl = new InlineLabel("fdr ≤");
+		fdrLbl.getElement().getStyle().setFloat(Float.NONE);
+		fdrPanel = new FlowPanel();
+		fdrPanel.getElement().getStyle().setFloat(Float.LEFT);
+		fdrPanel.add(fdrLbl);
+		fdrPanel.add(fdrInput);
+		controlPanel.add(fdrPanel);
+		fdrPanel.setVisible(true);
+		
+		super.add(controlPanel);
 		
 		eventBus.addHandler(SetFIFlagDataDescKeysEvent.TYPE, this);
 	}
@@ -76,11 +122,25 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 		History.newItem(helper.buildToken(tokenMap));
 	}
 	
+	/**
+	 * Update FLGFDR token and set new history token
+	 */
+	private void updateFDR() {
+		StateTokenHelper helper = new StateTokenHelper();
+		Map<String, String> tokenMap = helper.buildTokenMap(History.getToken());
+		tokenMap.put("FLGFDR", fdrInput.getValue()+"");
+		History.newItem(helper.buildToken(tokenMap));
+	}
+	
 	@Override
 	public void onNodeFlagged(NodeFlaggedEvent event) {
 		super.flagTerm = event.getTerm();
 		
 		super.includeInteractors = event.getIncludeInteractors();
+		
+		//controlPanel shouldnt be visible when there are no interactors
+		if(includeInteractors) controlPanel.setVisible(true);
+		else controlPanel.setVisible(false);
 		
 		String msg;
 		//There is a case where the DiagramObjectsFlaggedEvent gets fired with a null value for getFlaggedItems();
@@ -91,6 +151,10 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 		
 		super.loadingIcon.setVisible(false);
 		super.interactorsLabel.setVisible(false);
+		
+		StateTokenHelper helper = new StateTokenHelper();
+		fdrInput.setValue(helper.buildTokenMap(History.getToken()).get("FLGFDR"));
+		
 		setVisible(true);
 	}
 	
@@ -98,11 +162,11 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 	public void onSetFIFlagDataDescKeys(SetFIFlagDataDescKeysEvent event) {
 		this.dataDescKeys = event.getDataDescKeys();
 		if(dataDescKeys.contains(0)) {
-			prdInput.setVisible(true);
+			prdPanel.setVisible(true);
 			StateTokenHelper helper = new StateTokenHelper();
 			prdInput.setValue(helper.buildTokenMap(History.getToken()).get("SIGCUTOFF"));
 		}
-		else prdInput.setVisible(false);
+		else prdPanel.setVisible(false);
 	}
 	
 	public static IDGResources IDGRESOURCES;
@@ -124,5 +188,4 @@ public class IDGFlaggedItemsControl extends FlaggedItemsControl implements SetFI
 		
 		String prdInput();
 	}
-
 }
